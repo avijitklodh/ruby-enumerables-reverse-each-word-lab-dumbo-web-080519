e4d585fb05e2792c31575b4c7d122de0a61adfbc
Avijit Lodh <avijitklodh@gmail.com>
2019-07-25 08:50:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #def reverse_each_word(sentence)
 #arrayed = sentence.split (" ")
 #  a = []
@@ -13,6 +14,17 @@ def reverse_each_word(sentence)
 arrayed = sentence.split (" ")
 reversed = arrayed.collect {|i| i.reverse}
 reversed.join (" ")
+=======
+def reverse_each_word(sentence)
+arrayed = sentence.split (" ")
+  a = []
+  i = 0 
+  while i < arrayed.length do 
+    a << arrayed[i].reverse
+    i+=1
+  end
+  a.collect (" ")
+>>>>>>> 193c299153eb2103a0c7120593e677871e2d8fb7
 end
 
 
